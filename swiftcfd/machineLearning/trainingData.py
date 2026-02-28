b224128c47502095c9f39442968d52db7136aaa3
Tom-Robin Teschner <tomrobin.teschner@yahoo.de>
2026-02-28 11:39:55 +0000

    Added fields at n-2 to training data
    merge with previous commit

--- swiftcfd/machineLearning/trainingData.py
+++ swiftcfd/machineLearning/trainingData.py
@@ -18,6 +18,12 @@ class TrainingData:
 
         for var in self.training_variables:
             self.data[var] = {
+                f'{var}^n-2_i,j': [],
+                f'{var}^n-2_i-1,j': [],
+                f'{var}^n-2_i+1,j': [],
+                f'{var}^n-2_i,j-1': [],
+                f'{var}^n-2_i,j+1': [],
+
                 f'{var}^n-1_i,j': [],
                 f'{var}^n-1_i-1,j': [],
                 f'{var}^n-1_i+1,j': [],
@@ -43,6 +49,12 @@ class TrainingData:
     def commit_training_data(self):
         for var in self.training_variables:
             for (block, i, j) in self.mesh.internal_loop_all_blocks():
+                self.data[var][f'{var}^n-2_i,j'].append(self.field_manager.fields[var].oldoldold[block, i, j])
+                self.data[var][f'{var}^n-2_i-1,j'].append(self.field_manager.fields[var].oldoldold[block, i - 1, j])
+                self.data[var][f'{var}^n-2_i+1,j'].append(self.field_manager.fields[var].oldoldold[block, i + 1, j])
+                self.data[var][f'{var}^n-2_i,j-1'].append(self.field_manager.fields[var].oldoldold[block, i, j - 1])
+                self.data[var][f'{var}^n-2_i,j+1'].append(self.field_manager.fields[var].oldoldold[block, i, j + 1])
+
                 self.data[var][f'{var}^n-1_i,j'].append(self.field_manager.fields[var].oldold[block, i, j])
                 self.data[var][f'{var}^n-1_i-1,j'].append(self.field_manager.fields[var].oldold[block, i - 1, j])
                 self.data[var][f'{var}^n-1_i+1,j'].append(self.field_manager.fields[var].oldold[block, i + 1, j])
